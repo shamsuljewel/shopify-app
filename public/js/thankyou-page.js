2ab2c9c3bdd0e963ec1eff0fd918360662cf42b8
Shamsul Alam <shamsuljewel@gmail.com>
2020-09-28 17:56:16 +0200

    Update thankyou-page.js

--- public/js/thankyou-page.js
+++ public/js/thankyou-page.js
@@ -1,8 +1,10 @@
 //Check if we have Shopify Checkout Object Available
-var appUrl = "https://506db4a6.ngrok.io";
+var appUrl = "https://splugin.doorhub.io";
+/*
 if(Shopify.shop != 'doorhub-test.myshopify.com') {
     appUrl = 'https://dhs.gotomy.dev';
 }
+*/
 
 if (typeof Shopify !== 'undefined' && typeof Shopify.checkout !== 'undefined') {
     let orderId = Shopify.checkout.order_id;
